3660b2051d0fb8da5e244c798a1a40bf48efa044
Adarsh Kumar <adarshkumar9820@gmail.com>
2020-05-01 23:05:09 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    backgroundColor: Colors.blueGrey,
+    appBar: AppBar(
+      title: Center(child: Text("I am rich")),
+      backgroundColor: Colors.blueGrey[900],
+    ),
+    body: Center(
+      child: Image(
+        image: AssetImage('assets/images/iamrich.png'),
+      ),
+    ),
+  ),
+));
